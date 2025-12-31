9023047a6662be52608abb9804f7066c655f30c6
shiggsy365 <shiggsy365@gmail.com>
2025-12-31 19:28:55 +0000

    Delete opdsbrowser.koplugin directory

--- opdsbrowser.koplugin/_meta.lua
+++
@@ -1,6 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "opdsbrowser",
-    fullname = _("OPDS Browser with Ephemera"),
-    description = _([[Browse and download books from OPDS catalogs. Request new books via Ephemera integration.]]),
-}
